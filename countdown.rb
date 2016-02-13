5fec8535c3e0d55f9963cde29e7c2d53586ac63e
angyangie <angie.ramirez12@gmail.com>
2016-02-13 21:47:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
+def countdown (integer)
 
-def countdown
+  count = integer
+  while count >= 1
+    puts "#{count} SECOND(S)!"
+    count -=1
+  end
+
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (count2)
+
+  while count2 >= 1
+    puts "#{count2} SECOND(S)!"
+    sleep(1)
+    count2 -=1
+  end
+
+  return "HAPPY NEW YEAR!"
 end
